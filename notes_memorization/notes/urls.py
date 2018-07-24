7586b7e529dc9c60cbd7138ed4cd59fbb234c904
44Schwarz <sirmitya@gmail.com>
2018-07-24 21:56:29 +0300

    Move css to a separate file and add project selection to the index page

--- notes_memorization/notes/urls.py
+++ notes_memorization/notes/urls.py
@@ -1,8 +1,8 @@
-from django.urls import path, include
+from django.urls import path
 from . import views
 
 # app_name = 'notes'
 urlpatterns = [
     path(r'', views.index),
-    path(r'add_note/', views.add_note, name='addnote'),
+    path(r'add_note/', views.add_note),
 ]
--- notes_memorization/notes/views.py
+++ notes_memorization/notes/views.py
@@ -8,17 +8,20 @@ from django.utils import timezone
 # Create your views here.
 def index(request):
     notes = Note.objects.all()
+    projects = Project.objects.all()
     # notes = []
-    return render(request, 'notes/index.html', context={'notes': notes})
+    return render(request, 'notes/index.html', context={'notes': notes, 'projects': projects})
 
 
 @require_http_methods(["POST"])
 def add_note(request):
     try:
-        note = Note(text=request.POST['new_note'], project=Project.objects.get(pk=1), date_added=timezone.now())
+        note = Note(text=request.POST['new_note'],
+                    project=Project.objects.get(pk=request.POST['project_id']),
+                    date_added=timezone.now())
         note.save()
         text = 'Added a new note: %s (#%s)' % (note.text, note.id)
-        print(text)
+        print(text, note.project)
         return HttpResponse(text)
     except:
         return HttpResponse('Error', status=404)
