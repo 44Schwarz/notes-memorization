d15bb1b391f9a48cf991469e8e2c64939c545157
44Schwarz <sirmitya@gmail.com>
2018-08-29 15:37:49 +0300

    Apply a feature for notes review

--- notes_memorization/notes/urls.py
+++ notes_memorization/notes/urls.py
@@ -3,7 +3,8 @@ from . import views
 
 app_name = 'notes'
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.index, name='all_notes'),
     path('add_note/', views.add_note),
     path('<int:note_id>/', views.detail, name='detail'),
+    path('review/', views.review, name='review'),
 ]
--- notes_memorization/notes/views.py
+++ notes_memorization/notes/views.py
@@ -1,5 +1,9 @@
+from random import randint
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
+from django.urls import reverse
+from django.http import HttpResponse, JsonResponse
+from django.http.response import HttpResponseRedirect
+
 from .models import Label, Note, Project
 from django.views.decorators.http import require_http_methods
 from django.utils import timezone
@@ -9,7 +13,6 @@ from django.utils import timezone
 def index(request):
     notes = Note.objects.all()
     projects = Project.objects.all()
-    # notes = []
     return render(request, 'notes/index.html', context={'notes': notes, 'projects': projects})
 
 
@@ -22,7 +25,7 @@ def add_note(request):
         note.save()
         text = 'Added a new note: %s (#%s)' % (note.text, note.id)
         print(text, note.project)
-        return HttpResponse(text)
+        return HttpResponseRedirect('/notes')
     except:
         return HttpResponse('Error', status=404)
 
@@ -30,3 +33,24 @@ def add_note(request):
 def detail(request, note_id):
     note = get_object_or_404(Note, pk=note_id)
     return render(request, 'notes/detail.html', context={'note': note})
+
+
+@require_http_methods(["GET"])
+def review(request):
+    id_list = [n.id for n in Note.objects.all()]
+    cur_id = request.GET.get('cur_id')
+    if cur_id:  # get next note
+        if id_list.index(int(cur_id)) == len(id_list) - 1:
+            num = id_list[0]  # if the previous note was the last in list, get the first one
+        else:
+            num = id_list[id_list.index(int(cur_id)) + 1]
+        obj = Note.objects.get(pk=num)
+        obj.reviews_counter += 1
+        obj.save()
+        return JsonResponse({'note': str(obj), 'note_id': obj.id})
+    else:  # get random note
+        num = id_list[randint(0, len(id_list) - 1)]
+        obj = Note.objects.get(pk=num)
+        obj.reviews_counter += 1
+        obj.save()
+        return render(request, 'notes/review.html', context={'note': obj})
